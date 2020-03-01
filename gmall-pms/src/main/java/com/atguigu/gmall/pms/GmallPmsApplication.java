6bfcdc835bef158d5c40f7ea49e68f3db6e933b6
gaoxiaotong <599284555@qq.com>
2020-03-01 14:41:37 +0800

    项目逆向工程完成

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
@@ -1,9 +1,12 @@
 package com.atguigu.gmall.pms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableDubbo//启用dubbo
 @SpringBootApplication
+@MapperScan(basePackages = "com.atguigu.gmall.pms.mapper")//mybatis包扫描
 public class GmallPmsApplication {
 
     public static void main(String[] args) {
